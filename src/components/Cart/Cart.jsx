1fb2fde188c60568595fb950f8b63ecd9d162b50
lonkarpratiksha <lonkarpratiksha5@gmail.com>
2024-12-19 19:15:37 +0530

    Created CartItem from cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CartItem from './CartItem'
+import { Divider } from '@mui/material'
+
+
+const Cart = () => {
+    return (
+        <div>
+            <main className='lg:flex justify-between'>
+                <section className='lg:w-[30%] space-y-6 lg:min-h-screen pt-10'>
+                    <CartItem/>
+                </section>
+                <Divider/>
+            </main>
+        </div>
+    )
+}
+
+export default Cart
